497faa397df7352a150487bbec0d3bcc8454cfdb
Treidex <treidexsemextetry@gmail.com>
2021-04-04 11:10:13 -0700

    C# actually has API

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ralph
+{
+    class Program
+    {
+        DiscordSocketClient client;
+
+        static void Main(string[] args) =>
+            new Program().RalphBotAsync().GetAwaiter().GetResult();
+
+        async Task RalphBotAsync()
+        {
+            client = new DiscordSocketClient();
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string[] lines = File.ReadAllLines("Secret.txt");
+            string tok = lines[0];
+
+            await client.LoginAsync(TokenType.Bot, tok);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        Task MessageReceived(SocketMessage msg)
+        {
+            Console.WriteLine($"Recieved Message from '{msg.Author}': '{msg.Content}'");
+            return Task.CompletedTask;
+        }
+
+        Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
