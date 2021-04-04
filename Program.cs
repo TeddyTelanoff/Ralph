bcd8b6d8f558587cb7d67e98b27210bd54bef4b3
Treidex <treidexsemextetry@gmail.com>
2021-04-04 13:02:08 -0700

    ok, ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 
 using System;
@@ -9,16 +10,45 @@ namespace Ralph
 {
     public class Program
     {
-        private DiscordSocketClient client;
+        private static readonly string[] sadReplies = {
+            // Unlucky replies
+            "Be a man, and stop that yipper-yapping!",
+            "Suck it up.",
 
-        private static void Main(string[] args) =>
-            new Program().RalphBotAsync().GetAwaiter().GetResult();
+            // Positive replies
+            "Don't worry, everything is gonna be allright!",
+            "Here, have a lollypop :lollipop:",
+            "Don't be so sad, help is on the horizon",
+            "Studies show that taking a break can help clear your mind...",
+            "Studies show that taking a break can help clear your mind...",
+
+            // Acceptance
+            "Me too, kid. :cry:",
+        };
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private bool alive;
+
+        private static void Main(string[] args)
+        {
+            var prog = new Program();
+            var task = prog.RalphBotAsync();
+            while (prog.alive)
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    prog.alive = false;
+            task.Wait();
+        }
 
         private async Task RalphBotAsync()
         {
+            alive = true;
             client = new DiscordSocketClient();
             client.Log += Log;
-            client.MessageReceived += MessageReceived;
+            client.MessageReceived += HandleCommandAsync;
+
+            commands = new CommandService();
+            commands.CommandExecuted += ExecuteCommandAsync;
 
             string[] lines = File.ReadAllLines(@"..\..\..\Secret.txt");
             string tok = lines[0];
@@ -26,19 +56,43 @@ namespace Ralph
             await client.LoginAsync(TokenType.Bot, tok);
             await client.StartAsync();
 
-            await Task.Delay(-1);
+            // Wait
+            while (alive);
         }
 
-        private Task MessageReceived(SocketMessage msg)
+        private async Task ExecuteCommandAsync(Optional<CommandInfo> info, ICommandContext context, IResult result)
         {
-            if (!msg.Content.StartsWith("!"))
-                return Task.CompletedTask;
+            var channel = context.Channel;
 
-            string cmd = msg.Content.Substring(1);
-            var parser = new Parser(cmd);
-            Token[] toks = parser.Parse();
-            
-            return Task.CompletedTask;
+            // Get Command
+            int argPos = 0;
+            context.Message.HasCharPrefix('!', ref argPos);
+            string cmd = context.Message.Content.Substring(argPos);
+
+            switch (cmd.ToLower())
+            {
+            case "sad":
+                string reply = sadReplies[new Random().Next(sadReplies.Length)];
+                await channel.SendMessageAsync(reply);
+                break;
+            }
+        }
+
+        private async Task HandleCommandAsync(SocketMessage socketMsg)
+        {
+            var msg = socketMsg as SocketUserMessage;
+            if (msg is null)
+                return;
+
+            int argPos = 0;
+            if (!msg.HasCharPrefix('!', ref argPos))
+                return;
+
+            if (msg.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(client, msg);
+            await commands.ExecuteAsync(context, argPos, null);
         }
 
         private Task Log(LogMessage msg)
